066f4da24ab71ad54d01834b62a76a535a7ecbd4
constant94-dev <constant94@naver.com>
2023-09-27 01:15:35 +0900

    [0927] Day8_3 주석 수정 &  update READMD.md

--- Day8_3.java
+++ Day8_3.java
@@ -4,7 +4,7 @@ class Solution {
         // 가장 큰 수를 한번 찾는 반복문이 필요하다.
         // 이후의 가장 큰 수를 찾으려면 이전의 찾은 수를 제외하고 반복문을 실행한다.
         // 상태 값: (결과를 담을 배열, 가장 큰 수를 찾는 반복문을 실행할 배열, 가장 큰 수 초기값, 현재 응급도 순서, 응급실 환자 수),
-        // 종료 조건: 응급도 순서가 배열의 길이와 같을 때 종료,
+        // 종료 조건: 응급도 순서가 응급실 환자 수와 같을 때 종료,
         // 점화식: 어떻게 상태를 전이시켜 종료조건에 가깝게 할 것인가? 재귀를 수행할 때마다 응급도 순서가 증가한다.
         if (emergencyPriority == emergency_length) {
             // 마지막 응급도 순서를 세팅 후 리턴
@@ -17,15 +17,15 @@ class Solution {
         }
         
         int currentMax_idx = 0;
-        // 3. 해당 인덱스 번호를 결과 배열 인덱스의 번호를 매긴다.
+        // 3. 현재 비교하는 가장 큰 값 인덱스 번호를 결과 배열 인덱스의 현재 응급도 순서로 세팅한다.
         for (int i=0; i<emergency_length; i++){
             if (maxValue < emergency[i]){
                 maxValue = emergency[i];
                 currentMax_idx = i;
             }
         }
-        emergency[currentMax_idx] = 0; // 현재 가장높은 응급도 인덱스의 값을 '0' 변경
-        answer[currentMax_idx] = emergencyPriority; // 현재 응급도를 결과 배열 인덱스 번호의 저장
+        emergency[currentMax_idx] = 0; // 현재 가장높은 진료순서 인덱스의 값을 '0' 변경
+        answer[currentMax_idx] = emergencyPriority; // 현재 응급도를 가장 큰 값 인덱스로 결과 배열의 대입
         emergencyPriority++; // 다음 응급도 순서로 증가
         recur(answer,emergency,0,emergencyPriority,emergency_length);
         
