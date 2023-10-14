0dbb8a0338b16605184b4c350871565a7b4d629f
constant94-dev <constant94@naver.com>
2023-10-14 23:08:03 +0900

    [1014] main 함수 작성

--- Day22_3.java
+++ Day22_3.java
@@ -1,3 +1,5 @@
 public class Day22_3 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- Day22_4.java
+++ Day22_4.java
@@ -1,3 +1,5 @@
 public class Day22_4 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
