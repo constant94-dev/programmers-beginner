708aaa9fee958a60f67d16bb07f4c2b108d7c2a3
constant94-dev <constant94@naver.com>
2023-10-15 11:03:05 +0900

    [1015] Day22 문제풀이 및 오류케이스 분석 & update README.md

--- Day22_3.java
+++ Day22_3.java
@@ -1,5 +1,38 @@
 public class Day22_3 {
-    public static void main(String[] args) {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int[] overlapLine = new int[201];
+        // 1. 입력된 2차월 배열 길이만큼 순회
+        int length = lines.length;
+        for (int i=0; i<length; i++){
+            overlapLine = overLap(lines[i][0], lines[i][1], overlapLine); // 2. 순회시마다 선분 그리기
+        }
         
+        // 5. 순회가 끝난 후 겹쳐진 선분 찾기
+        int overlap_length = overlapLine.length;
+        for (int i=0; i<overlap_length; i++){
+            if (overlapLine[i] >= 2){
+                answer++;
+            }
+        }
+        System.out.println("결과는: "+answer);
+        // 6. 위 과정을 마무리한 후 결과 리턴
+        return answer;
+    }
+    
+    private int[] overLap(int start, int end, int[] overlapLine) {
+        start = start+100; // 선분 시작점을 음수가 아닌 양수만 사용하기 위함
+        end = end+100; // 선분 끝점을 음수가 아닌 양수만 사용하기 위함
+        // 3. 인자값의 start부터 end전 까지 순회
+        for(int i=start; i<end; i++){
+            // 4. 순회시마다 각 인덱스 번호를 찾아가 1 증가
+            overlapLine[i]++;
+        }
+        return overlapLine;
+    }
+
+    public static void main(String[] args) {
+        int[][] lines = {{0, 1}, {2, 5}, {3, 9}};
+        new Day22_3().solution(lines);
     }
 }
--- Day22_4.java
+++ Day22_4.java
@@ -1,5 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day22_4 {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int common = 0;
+        List<Integer> numbers = new ArrayList<>();
+        // 1. 입력된 정수 a,b의 최대공약수를 구한다.
+        common = gcd(a, b);
+        // 2. 기약분수를 만든다. (최대공약수로 분자와 분모를 나누면 기약분수다.)
+        a = a/common;
+        b = b/common;
+        if (b == 1){
+            answer = 1;
+        }
+        // 3. 기약분수의 분모의 소인수가 2 또는 5라면 유한소수다.
+        numbers = smallNum(b);
+        for (int num : numbers){
+            System.out.println("현재 번호는: "+num);
+            if (num == 2 || num == 5){
+                answer = 1;
+            } else {
+                answer = 2;
+                break;
+            }
+        }
+        System.out.println("결과는: "+answer);
+        // 4. 위 과정을 마무리한 후 결과 리턴
+        return answer;
+    }
+    private List<Integer> smallNum(int b) { // 소인수분해
+        List<Integer> numbers = new ArrayList<>();
+        int idx = 2;
+        while (true) {
+            if (b == 1){
+                return numbers;
+            }
+
+            if (b%idx == 0){
+                b = b/idx;
+                numbers.add(idx);
+            } else {
+                idx++;
+            }
+        }
+    }
+    private int gcd(int a, int b) { // 유클리드 호제법
+        if (a%b == 0) return b;
+
+        return gcd(b, a%b);
+    }
     public static void main(String[] args) {
-        
+        new Day22_4().solution(14, 15);
     }
 }
